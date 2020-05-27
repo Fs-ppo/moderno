feaf6950c132ff4441f5ce0c11f57e7ec83c936a
fs-ppo <fs_ppo@mail.ru>
2020-05-27 22:25:40 +0600

    added products-slider section

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,22 @@
 $(function(){
 
 
+
     var mixer = mixitup('.products__inner-wrapper');
 
+    $(".products-slider__inner").slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        arrows: false
+    });
 
+    $(".rate_star").rateYo({
+        rating: 5,
+        starWidth: "12px",
+        readOnly: true
+      });
 
 
 
